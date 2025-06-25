d9b0ad999e3ad4374d8f4e09f96fbb2f9ed11d4a
Ivo Mirchev <extern.ivo.mirchev@aldi-sued.com>
2025-06-25 13:36:00 +0300

    chore: updated terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,23 +3,6 @@ variable "config" {
   type        = any
 }
 
-variable "SSH_PUBLIC_KEY" {
-  description = "Public SSH key for EC2 access"
-  type        = string
-  sensitive   = true
-}
-
-variable "DB_USERNAME" {
-  description = "Database username, stored securely in GitHub Repository secrets"
-  type        = string
-}
-
-variable "DB_PASSWORD" {
-  description = "Database password, stored securely in GitHub Repository secrets"
-  type        = string
-  sensitive   = true
-}
-
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS Access Key ID for Terraform authentication"
   type        = string
@@ -30,20 +13,4 @@ variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Access Key for Terraform authentication"
   type        = string
   sensitive   = true
-}
-
-variable "ADMIN_USER" {
-  description = "WordPress admin username"
-  type        = string
-}
-
-variable "ADMIN_PASS" {
-  description = "WordPress admin password"
-  type        = string
-  sensitive   = true
-}
-
-variable "ADMIN_EMAIL" {
-  description = "WordPress admin email address"
-  type        = string
 }
